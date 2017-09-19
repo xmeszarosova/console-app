6220b179b392516e4bb0676e1ceb5266bd219197
xmeszarosova <meszarosova.rita@gmail.com>
2017-09-19 09:38:58 +0200

    cviko

--- main.cpp
+++ main.cpp
@@ -5,7 +5,26 @@
 
 int main()
 {
+	File*subor;
 	std::cout << "Hello zadania!" << std::endl;
+	char prepinac;
+
+	StreamReader* subor = new StreamReader("c:\\text.txt");
+	String *riadok = "";
+	ArrayList *text = new ArrayList();
+
+	std::cout << "Prosim zvolte prepinaca: c, w, l" << std::endl;
+	std::cin >> prepinac;
+
+	if (prepinac == "l") {
+		riadok = subor->ReadLine(); while (riadok != 0)
+		{
+			text->Add(riadok);
+			riadok = subor->ReadLine(); <BR / >
+		}
+	}
+
+	subor->Close();
 
 	return 0;
 }
