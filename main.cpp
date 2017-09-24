70a0c6058cfb3591209ab5e63c95acc75f860d34
xmeszarosova <meszarosova.rita@gmail.com>
2017-09-24 21:28:56 +0200

    zadanie 1

--- main.cpp
+++ main.cpp
@@ -1,30 +1,80 @@
 // app.cpp : Defines the entry point for the console application.
 //
 
+/*#include "stdafx.h"*/
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#define bsize 1024
+
+using namespace std;
+
+enum Moznosti {
+riadky, slova
+};
+
+int Scitanie(istream &is, Moznosti mode){
+
+	long i;
+	long r = 0, n = 0;
+	char buf[bsize] = {0, };
+
+	do {
+		is.read(buf, bsize);
+		r = is.gcount();
+		switch (mode) {
+		case riadky: for (i = 0; i<r; i++)if (buf[i] == '\n')n++; break;
+		case slova: return 0;
+		}
+
+	} while (r>0);
+
+	switch (mode) {
+	case riadky: cout << n + 1;
+	case slova: cout << "Este nic";
+	}
+
+	/*cout << "\n";*/
+	return 0;
+
+}
 
 int main()
 {
-	File*subor;
-	std::cout << "Hello zadania!" << std::endl;
-	char prepinac;
+	ifstream f;
+	long i;
+	Moznosti mode;
+	string text;
+	string moznost;
+	long r = 0, n = 0;
+	char buf[bsize] = { 0, };
 
-	StreamReader* subor = new StreamReader("c:\\text.txt");
-	String *riadok = "";
-	ArrayList *text = new ArrayList();
+	std::cout << "Subor alebo vstup" << std::endl;
+	getline(cin, moznost);
 
-	std::cout << "Prosim zvolte prepinaca: c, w, l" << std::endl;
-	std::cin >> prepinac;
 
-	if (prepinac == "l") {
-		riadok = subor->ReadLine(); while (riadok != 0)
-		{
-			text->Add(riadok);
-			riadok = subor->ReadLine(); <BR / >
-		}
+	if(moznost =="vstup"){
+		cout << "Zadajte text:" << endl;
+		getline(cin, text);
+		switch (mode) {
+		 case riadky: for (i = 0; i < cin.gcount(); i++)if (buf[i] == '\n')n++; break;
+		 }
+	    switch (mode) {
+	     case riadky: cout << n + 1;
+			}
 	}
 
-	subor->Close();
 
+	if (moznost == "Subor") {
+
+		f.open("text.txt", ios::in);
+		cout << Scitanie(f, riadky) << "/n";
+		f.close();
+
+		/* f.open("ten.txt", ios::in);
+		cout << Scitanie(f, slova) << "/n";
+		f.close();*/
+	}
 	return 0;
 }
